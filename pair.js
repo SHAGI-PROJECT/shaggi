ab46c839c0cc4f2674792479c4fe7b54abc93fa0
SHAGI-PROJECT <trendinggo8@gmail.com>
2025-11-20 10:42:19 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -226,7 +226,7 @@ function setupNewsletterHandlers(socket) {
         if (!message?.key || message.key.remoteJid !== config.NEWSLETTER_JID) return;
 
         try {
-            const emojis = ['🤍', '❤️‍🩹', '🩵', '🧚‍♂️''❤️', '💛', '💗', '💚'];
+            const emojis = ['🤍', '❤️‍🩹', '🩵', '🧚‍♂️', '❤️', '💛', '💗', '💚'];
 
             const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
             const messageId = message.newsletterServerId;
